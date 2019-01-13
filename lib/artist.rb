549fb25304ad0af59a400e263401c7b0cac1fe4b
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-01-13 21:51:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,44 +1,21 @@
-#class Artist 
- # attr_accessor :name, :genre
-  #@@all = []
+class Artist 
+  attr_accessor :name, :genre
+  @@all = []
   
-  #def initialize(name)
-   # @name = name 
-    #@@all << self
-#  end
-  
- # def self.all
-  #  @@all
-#  end
-  
- # def new_song(song)
-  #   @songs << song
-   # song.artist = self
-    
-#  end
-  
-#end
-
-class Artist
-  attr_accessor :name
-
   def initialize(name)
-    @name = name
-    @songs = []
+    @name = name 
+    @@all << self
   end
-
-  def add_song(song)
-    @songs << song
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(song)
+     @songs << song
     song.artist = self
+    
   end
-
-  def songs
-    @songs
-  end
-
-  def genres
-    @songs.collect do |song|
-      song.genre
-    end
-  end
+  
 end
+
